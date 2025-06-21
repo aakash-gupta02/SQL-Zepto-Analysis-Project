794d907df8850948c7ee40f6eabd25a5d84772f3
Aakash <editsky597@gmail.com>
2025-06-21 11:53:51 +0530

    main sql

---
+++ main.sql
@@ -0,0 +1,34 @@
+create database if not exists zepto;
+
+use zepto;
+
+CREATE TABLE zeptodata2 (
+  category VARCHAR(120),
+  name VARCHAR(150) NOT NULL,
+  mrp DECIMAL(8,2),
+  discountPercent DECIMAL(5,2),
+  availableQuantity INT,
+  discountedSellingPrice DECIMAL(8,2),
+  weightInGms INT,
+  outOfStock BOOLEAN,
+  quantity INT
+);
+
+
+CREATE TABLE zeptodata (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category VARCHAR(120),
+  name VARCHAR(150) NOT NULL,
+  mrp DECIMAL(8,2),
+  discountPercent DECIMAL(5,2),
+  availableQuantity INT,
+  discountedSellingPrice DECIMAL(8,2),
+  weightInGms INT,
+  outOfStock BOOLEAN,
+  quantity INT
+);
+
+
+
+select * from zeptodata;
+
